96a759928be438b8c8e7c1ab2caf93e6711b4807
CompactLethargy13 <107340414+CompactLethargy13@users.noreply.github.com>
2022-06-27 17:46:45 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+Events.on(ClientLoadEvent, b  => {
+Blocks.arc.targetAir = true;
+Bullets.damageLightningGround.collidesAir = true;
+Blocks.arc.shootType.lightningLength = 30
+Blocks.arc.range=130
+});
